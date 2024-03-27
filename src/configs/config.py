5fb757e493e7fc243f27fc5b9de49cbce4527dca
Nikita Smolin <nikitkasmo@gmail.com>
2024-03-27 16:10:43 +0300

    added logic to load variables from environment

--- src/configs/config.py
+++ src/configs/config.py
@@ -1,17 +1,35 @@
-import ujson
 import schemas.bot.bot_properties as bot_properties
-
-from tools.converters import Url
+import ujson
+import os
 
 
 class Config:
+    """
+    TODO:
+     - REMOVE DEPRECATED MENTIONS OF SECRET.JSON AND USELESS CLASSMETHODS
+    """
+
+    token: str = os.getenv("TOKEN", "")
+    db_url: str = os.getenv("DATABASE_URL", "")
+    test_guild: str = os.getenv("TEST_GUILD", "")
+
+    """
+    REMOVE THE STUFF BELOW, ITS USELESS!!!!! AND THE Config CLASS IS USELESS TOO.
+    JUST CREATE VARIABLES INSTEAD OF USING CLASSES
+    """
+
     with open("configs/secret.json", mode="r", encoding="utf-8") as _file:
         _data = ujson.load(_file)
 
     @classmethod
     def get_bot(cls) -> bot_properties.Bot:
-        return bot_properties.Bot(token=cls._data["token"], database_url=cls._data["databaseUrl"], test_guild=int(cls._data["testGuild"]))
-    
+        return bot_properties.Bot(
+            token=cls._data["token"],
+            database_url=cls._data["databaseUrl"],
+            test_guild=int(cls._data["testGuild"]),
+        )
+
     @classmethod
     def get_url(cls):
         return cls._data["databaseUrl"]
+
